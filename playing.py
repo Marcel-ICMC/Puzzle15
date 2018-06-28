8725212bdc1724af76fbd276f88584b3f7cb868a
Marcel-ICMC <marcel.lima@usp.br>
2018-06-28 08:47:11 -0300

    terminando solver por exaustivo

--- playing.py
+++ playing.py
@@ -1,5 +1,45 @@
+from collections import deque
 import puzzle15
 
-x = puzzle15.puzzle()
-x.shuffle()
-x.printPuzzle()
+
+class Solver():
+    def __init__(self):
+        self.puzzle = puzzle15.puzzle()
+    
+    def recreate(original):
+        new = Solver()
+        new.puzzle.copy(original.puzzle)
+        return new
+
+    def Solve(self):
+        queue = deque([self])
+        aux = Solver()
+        aux.puzzle.copy(queue.popleft().puzzle)
+        
+        while (aux.puzzle.isSolved() == 0):
+            if aux.puzzle.moveDown():
+                queue.append(Solver.recreate(aux))
+                aux.puzzle.moveUp()
+            
+            if aux.puzzle.moveUp():
+                queue.append(Solver.recreate(aux))
+                aux.puzzle.moveDown()
+            
+            if aux.puzzle.moveLeft():
+                queue.append(Solver.recreate(aux))
+                aux.puzzle.moveRight()
+            
+            if aux.puzzle.moveRight():
+                queue.append(Solver.recreate(aux))
+                aux.puzzle.moveLeft()
+        
+            aux.puzzle.copy(queue.popleft().puzzle)
+        
+        return aux
+
+x = Solver()
+x.puzzle.Shuffle()
+x.puzzle.printPuzzle()
+print()
+x.Solve()
+x.puzzle.printPuzzle()
--- puzzle15.py
+++ puzzle15.py
@@ -3,22 +3,42 @@ import random
 #A ideia neste codigo eh implementar o jogo dos 15 ou 15 puzzle
 class puzzle():
     #0 eh a posicao livre
-    puzzle = ([1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0])
-    empty = [3, 3]#ordem em linha, coluna
+    def __init__(self):
+        self.puzzle = ([1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0])
+        self.empty = [3, 3]#ordem em linha, coluna
 
+    def getPuzzle(self):
+        return self.puzzle
+
+    def getEmpty(self):
+        return self.empty
+
+    def copy(self, original):
+        for line in range(0, 4):
+            for column in range(0, 4):
+                self.puzzle[line][column] = original.puzzle[line][column]
+        self.empty[0] = original.empty[0]
+        self.empty[1] = original.empty[1]
+
+
+    #Os quatro metodos de movimentacao retornam 1 se moveu e 0 se nao eh possivel mover
     #move a posicao vazia para a direita
     def moveRight(self):
         if self.empty[1] != 3:
             self.puzzle[self.empty[0]][self.empty[1]] = self.puzzle[self.empty[0]][self.empty[1] + 1]
             self.puzzle[self.empty[0]][self.empty[1] + 1] = 0
             self.empty[1] = self.empty[1] + 1
-
+            return 1
+        return 0
+    
     #move a posicao vazia para a esquerda
     def moveLeft(self):
         if self.empty[1] != 0:
             self.puzzle[self.empty[0]][self.empty[1]] = self.puzzle[self.empty[0]][self.empty[1] - 1]
             self.puzzle[self.empty[0]][self.empty[1] - 1] = 0
             self.empty[1] = self.empty[1] - 1
+            return 1
+        return 0
 
     #move a posicao vazia para cima
     def moveUp(self):
@@ -26,16 +46,20 @@ class puzzle():
             self.puzzle[self.empty[0]][self.empty[1]] = self.puzzle[self.empty[0] - 1][self.empty[1]]
             self.puzzle[self.empty[0] - 1][self.empty[1]] = 0
             self.empty[0] = self.empty[0] - 1
-    
+            return 1
+        return 0
+
     #move a posicao vazia para baixo
     def moveDown(self):
         if self.empty[0] != 3:
             self.puzzle[self.empty[0]][self.empty[1]] = self.puzzle[self.empty[0] + 1][self.empty[1]]
             self.puzzle[self.empty[0] + 1][self.empty[1]] = 0
             self.empty[0] = self.empty[0] + 1
+            return 1
+        return 0
 
     #mistura todo o quebra-cabeca
-    def shuffle(self):
+    def Shuffle(self):
         for moves in range(0, 100):
             rand = random.randint(0,3)
             if rand == 0:
@@ -47,9 +71,14 @@ class puzzle():
             if rand == 3:
                 self.moveDown()
 
+    def isSolved(self):
+        if(self.puzzle == ([1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0])):
+            return 1
+        return 0
+
     #mostra o atual quebra-cabeca na tela
     def printPuzzle(self):
         for line in range(0, 4):
             for column in range(0, 4):
-                print(str(self.puzzle[line][column]), end = ' ')
+                print(str(self.puzzle[line][column]), end = " ")
             print()
