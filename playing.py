323026478d9db86c9e1a62e2567e37ea0925697a
Marcel-ICMC <marcel.lima@usp.br>
2018-06-29 10:51:57 -0300

    adicionando heuristica

--- playing.py
+++ playing.py
@@ -1,73 +1,120 @@
+from queue import PriorityQueue
 from collections import deque
 import puzzle15
 
 
 class Solver():
-    def __init__(self):
-        self.puzzle = puzzle15.puzzle()
-        self.moves = "FLAG"
+	def __init__(self):
+		self.puzzle = puzzle15.puzzle()
+		self.moves = "FLAG"
 
-    #retorna a string com todos os movimentos feitos
-    def getMoves(self):
-        return self.moves
+	#retorna a string com todos os movimentos feitos
+	def getMoves(self):
+		return self.moves
 
-    def addMove(self, move):
-        self.moves = self.moves + move
+	#adiciona um movimento a string moves
+	def addMove(self, move):
+		self.moves = self.moves + move
 
-    def deleteMove(self):
-        self.moves = self.moves[:- 1]
+	#deleta o ultimo movimento feito
+	def deleteMove(self):
+		self.moves = self.moves[:- 1]
 
-    #recria um solver e retorna a copia
-    def recreate(original):
-        new = Solver()
-        new.puzzle.copy(original.puzzle)
-        new.moves = original.moves
+	#recria um solver e retorna a copia
+	def recreate(original):
+		new = Solver()
+		new.puzzle.copy(original.puzzle)
+		new.moves = original.moves
+		return new
 
-        return new
+	#printa o estado em que a solucao esta
+	def printSolution(self):
+		print("It takes " + str(len(self.moves) - 4) + " moves  " + self.moves[4:])
+		self.puzzle.printPuzzle()
 
-    #printa o estado em que a solucao esta
-    def printSolution(self):
-        print("It takes " + str(len(self.moves) - 4) + "moves  " + self.moves[4:])
-        self.puzzle.printPuzzle()
+	#retorna quantos estao fora de sua posicao
+	def heuristicSolution(self):
+		counter = 0
+		for line in range(0, 4):
+			for column in range(0,4):
+				if self.puzzle.puzzle[line][column] == (line*4) + column + 1:
+					counter += 1
+		return counter + len(self.moves)
 
-    def Solve(self):
-        queue = deque([self])
-        aux = Solver()
-        aux.puzzle.copy(queue.popleft().puzzle)
+	#solver de forca bruta
+	def BFSolver(self):
+		queue = deque([self])
+		aux = Solver()
+		aux.puzzle.copy(queue.popleft().puzzle)
         
-        while (aux.puzzle.isSolved() == 0):
-            if aux.moves[-1] != 'U' and aux.puzzle.moveDown():
-                aux.addMove("D")
-                queue.append(Solver.recreate(aux))
-                aux.puzzle.moveUp()
-                aux.deleteMove()
+		while (aux.puzzle.isSolved() == 0):
+			if aux.moves[-1] != 'U' and aux.puzzle.moveDown():
+				aux.addMove("D")
+				queue.append(Solver.recreate(aux))
+				aux.puzzle.moveUp()
+				aux.deleteMove()
             
-            if aux.moves[-1] != 'D' and aux.puzzle.moveUp():
-                aux.addMove("U")
-                queue.append(Solver.recreate(aux))
-                aux.deleteMove()
-                aux.puzzle.moveDown()
+			if aux.moves[-1] != 'D' and aux.puzzle.moveUp():
+				aux.addMove("U")
+				queue.append(Solver.recreate(aux))
+				aux.deleteMove()
+				aux.puzzle.moveDown()
             
-            if aux.moves[-1] != 'R' and aux.puzzle.moveLeft():
-                aux.addMove("L")
-                queue.append(Solver.recreate(aux))
-                aux.deleteMove()
-                aux.puzzle.moveRight()
+			if aux.moves[-1] != 'R' and aux.puzzle.moveLeft():
+				aux.addMove("L")
+				queue.append(Solver.recreate(aux))
+				aux.deleteMove()
+				aux.puzzle.moveRight()
             
-            if aux.moves[-1] != 'L' and aux.puzzle.moveRight():
-                aux.addMove("R")
-                queue.append(Solver.recreate(aux))
-                aux.deleteMove()
-                aux.puzzle.moveLeft()
+			if aux.moves[-1] != 'L' and aux.puzzle.moveRight():
+				aux.addMove("R")
+				queue.append(Solver.recreate(aux))
+				aux.deleteMove()
+				aux.puzzle.moveLeft()
         
-            aux = Solver.recreate(queue.popleft())
+			aux = Solver.recreate(queue.popleft())
+		return aux
+
+	def heuristicSolver(self):
+		heap = PriorityQueue()
+		aux = []
+		heap.put((int(self.heuristicSolution()), Solver.recreate(self)))
+		aux = heap.get()
+	
+		while True:
+			if aux[1].moves[-1] != 'U' and aux[1].puzzle.moveDown():
+				aux[1].addMove("D")
+				heap.put((int(aux[1].heuristicSolution()), Solver.recreate(aux[1])))
+				aux[1].puzzle.moveUp()
+				aux[1].deleteMove()
             
+			if aux[1].moves[-1] != 'D' and aux[1].puzzle.moveUp():
+				aux[1].addMove("U")
+				heap.put((int(aux[1].heuristicSolution()), Solver.recreate(aux[1])))
+				aux[1].deleteMove()
+				aux[1].puzzle.moveDown()
+            
+			if aux[1].moves[-1] != 'R' and aux[1].puzzle.moveLeft():
+				aux[1].addMove("L")
+				heap.put((int(aux[1].heuristicSolution()), Solver.recreate(aux[1])))
+				aux[1].deleteMove()
+				aux[1].puzzle.moveRight()
+            
+			if aux[1].moves[-1] != 'L' and aux[1].puzzle.moveRight():
+				aux[1].addMove("R")
+				heap.put((int(aux[1].heuristicSolution()), Solver.recreate(aux[1])))
+				aux[1].deleteMove()
+				aux[1].puzzle.moveLeft()
+			
+			aux = heap.get()		
+			if aux[1].puzzle.isSolved():
+				break
+		return aux
 
-        return aux
-
+	    
 x = Solver()
 x.puzzle.Shuffle()
 x.puzzle.printPuzzle()
 print()
-solved = x.Solve()
+solved = x.heuristicSolver()
 solved.printSolution()
