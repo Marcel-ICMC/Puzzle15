f7eed7c59ba64a4580db7349252bf991cac8f04d
Marcel-ICMC <marcel.lima@usp.br>
2018-06-29 07:27:31 -0300

    nada de mais

--- playing.py
+++ playing.py
@@ -5,7 +5,7 @@ import puzzle15
 class Solver():
     def __init__(self):
         self.puzzle = puzzle15.puzzle()
-        self.moves = ""
+        self.moves = "FLAG"
 
     #retorna a string com todos os movimentos feitos
     def getMoves(self):
@@ -17,7 +17,7 @@ class Solver():
     def deleteMove(self):
         self.moves = self.moves[:- 1]
 
-    #recria um solver se retorna a copia
+    #recria um solver e retorna a copia
     def recreate(original):
         new = Solver()
         new.puzzle.copy(original.puzzle)
@@ -27,7 +27,7 @@ class Solver():
 
     #printa o estado em que a solucao esta
     def printSolution(self):
-        print(self.moves)
+        print("It takes " + str(len(self.moves) - 4) + "moves  " + self.moves[4:])
         self.puzzle.printPuzzle()
 
     def Solve(self):
@@ -36,25 +36,25 @@ class Solver():
         aux.puzzle.copy(queue.popleft().puzzle)
         
         while (aux.puzzle.isSolved() == 0):
-            if aux.puzzle.moveDown():
+            if aux.moves[-1] != 'U' and aux.puzzle.moveDown():
                 aux.addMove("D")
                 queue.append(Solver.recreate(aux))
                 aux.puzzle.moveUp()
                 aux.deleteMove()
             
-            if aux.puzzle.moveUp():
+            if aux.moves[-1] != 'D' and aux.puzzle.moveUp():
                 aux.addMove("U")
                 queue.append(Solver.recreate(aux))
                 aux.deleteMove()
                 aux.puzzle.moveDown()
             
-            if aux.puzzle.moveLeft():
+            if aux.moves[-1] != 'R' and aux.puzzle.moveLeft():
                 aux.addMove("L")
                 queue.append(Solver.recreate(aux))
                 aux.deleteMove()
                 aux.puzzle.moveRight()
             
-            if aux.puzzle.moveRight():
+            if aux.moves[-1] != 'L' and aux.puzzle.moveRight():
                 aux.addMove("R")
                 queue.append(Solver.recreate(aux))
                 aux.deleteMove()
